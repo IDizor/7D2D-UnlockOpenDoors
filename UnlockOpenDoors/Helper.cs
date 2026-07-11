7db4454b8d8ec0a98f74db38bf57652e0035daba
IDizor <idizorr@gmail.com>
2026-07-11 18:50:48 +0300

    Version 3.0.1

---
+++ UnlockOpenDoors/Helper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+using UniLinq;
+using UnityEngine;
+
+namespace UnlockOpenDoors
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// Gets call stack methods order.
+        /// </summary>
+        public static string GetCallStackPath(int limit = 5)
+        {
+            var stackTrace = new System.Diagnostics.StackTrace();
+            var path = string.Join(" <- ", stackTrace.GetFrames()
+                .Skip(4)
+                .Take(limit)
+                .Select(f => f.GetMethod())
+                .Select(m =>
+                {
+                    var match = Regex.Match(m.Name, @".*?\s(\w+:\w+)\(.+");
+                    if (match.Success)
+                    {
+                        return $"{match.Groups[1].Value}()";
+                    }
+                    return $"{m.DeclaringType.Name}.{m.Name}()";
+                }));
+
+            return path;
+        }
+
+        public static string ToCompasPos(Vector3i p)
+        {
+            return (Math.Abs(p.x).ToString() + (p.x > 0 ? "E" : "W")) + ", " + (Math.Abs(p.z).ToString() + (p.z > 0 ? "N" : "S")) + ", " + p.y.ToString() + "h";
+        }
+
+        public static bool IsLockableDoor(Block block)
+        {
+            var composite = block.Properties.GetClass("CompositeFeatures");
+            return composite != null && composite.Classes.ContainsKey(nameof(TEFeatureDoor)) && composite.Classes.ContainsKey(nameof(TEFeatureLockable));
+        }
+
+        public static bool IsDoorVisuallyOpen(byte blockValue_meta)
+        {
+            return (blockValue_meta & 1) != 0;
+        }
+
+        public static bool IsDoorLocked(byte blockValue_meta)
+        {
+            return (blockValue_meta & 4) != 0;
+        }
+    }
+}
--- UnlockOpenDoors/UnlockOpenDoors.cs
+++ UnlockOpenDoors/UnlockOpenDoors.cs
@@ -1,115 +1,98 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using HarmonyLib;
-using UniLinq;
-using UnityEngine;
 
-/// <summary>
-/// Makes locked open doors unlocked.
-/// </summary>
-public class UnlockOpenDoors : IModApi
+namespace UnlockOpenDoors
 {
-    public void InitMod(Mod _modInstance)
-    {
-        var harmony = new Harmony(GetType().ToString());
-        harmony.PatchAll(Assembly.GetExecutingAssembly());
-    }
-
-    public static bool IsDoorOpen(byte _metadata)
-    {
-        return (_metadata & 1) != 0;
-    }
-
     /// <summary>
-    /// Makes locked open doors/hatches/gates unlocked.
+    /// Makes locked open doors unlocked.
     /// </summary>
-    [HarmonyPatch(typeof(TEFeatureLockable))]
-    [HarmonyPatch(nameof(TEFeatureLockable.SetLocked))]
-    public static class TEFeatureLockable_SetLocked
+    public class UnlockOpenDoors : IModApi
     {
-        public static void Prefix(TEFeatureLockable __instance, ref bool _isLocked)
+        public void InitMod(Mod _modInstance)
         {
-            if (_isLocked)
+            var harmony = new Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        /// <summary>
+        /// Makes locked open doors/hatches/gates unlocked.
+        /// </summary>
+        [HarmonyPatch(typeof(TEFeatureLockable), nameof(TEFeatureLockable.SetLocked))]
+        public static class TEFeatureLockable_SetLocked
+        {
+            public static void Prefix(TEFeatureLockable __instance, ref bool _isLocked)
             {
-                if (__instance.TryGetSelfOrFeature<TEFeatureDoor>(out var _) && IsDoorOpen(__instance.blockValue.meta))
+                if (_isLocked && __instance.GetOwner()== null)
                 {
-                    //Debug.LogError($"(SetLocked) Annoying door: {__instance.blockValue.Block.GetBlockName()}, Position: {ToCompasPos(__instance.ToWorldPos())}");
-                    _isLocked = false;
+                    if (__instance.Parent.TryGetSelfOrFeature<TEFeatureDoor>(out var door) && door.IsOpen())
+                    {
+                        //Debug.LogError($"(SetLocked) Annoying door: {__instance.blockValue.Block.blockName}, Position: {Helper.ToCompasPos(__instance.ToWorldPos())}");
+                        _isLocked = false;
+                    }
                 }
             }
         }
-    }
 
-    /// <summary>
-    /// Makes locked open doors/hatches/gates unlocked (for already discovered doors).
-    /// </summary>
-    [HarmonyPatch(typeof(TileEntity))]
-    [HarmonyPatch(nameof(TileEntity.OnReadComplete))]
-    public static class TileEntity_OnReadComplete
-    {
-        public static void Postfix(TileEntity __instance)
+        /// <summary>
+        /// Makes locked open doors/hatches/gates unlocked (for already discovered doors).
+        /// </summary>
+        [HarmonyPatch(typeof(TileEntity), nameof(TileEntity.OnReadComplete))]
+        public static class TileEntity_OnReadComplete
         {
-            if (__instance.TryGetSelfOrFeature<TEFeatureDoor>(out var door) && door.lockFeature != null && door.lockFeature.IsLocked())
+            public static void Postfix(TileEntity __instance)
             {
-                if (IsDoorOpen(__instance.blockValue.meta) && door.lockFeature.GetOwner() == null)
+                if (__instance.TryGetSelfOrFeature<TEFeatureDoor>(out var door) && door.IsOpen() && door.lockFeature != null && door.lockFeature.IsLocked() && door.lockFeature.GetOwner() == null)
                 {
-                    //Debug.LogErrorFormat($"(OnReadComplete) Annoying door: {__instance.blockValue.Block.GetBlockName()}, Position: {ToCompasPos(door.ToWorldPos())}");
+                    //Debug.LogErrorFormat($"(OnReadComplete) Annoying door: {__instance.blockValue.Block.blockName}, Position: {Helper.ToCompasPos(door.ToWorldPos())}");
                     door.lockFeature.SetLocked(false);
                 }
             }
         }
-    }
 
-    /// <summary>
-    /// Unlocks door/hatch/gates opened by a key or switch.
-    /// </summary>
-    [HarmonyPatch(typeof(TEFeatureLockable))]
-    [HarmonyPatch(nameof(TEFeatureLockable.OnBlockTriggered))]
-    public static class TEFeatureLockablee_OnBlockTriggered
-    {
-        public static void Postfix(TEFeatureLockable __instance, BlockValue _blockValue)
+        /// <summary>
+        /// Unlocks door/hatch/gates opened by a key or switch.
+        /// </summary>
+        [HarmonyPatch(typeof(TEFeatureLockable), nameof(TEFeatureLockable.OnBlockTriggered))]
+        public static class TEFeatureLockablee_OnBlockTriggered
         {
-            if (IsDoorOpen(_blockValue.meta) && __instance.TryGetSelfOrFeature<TEFeatureDoor>(out var _))
+            public static void Postfix(TEFeatureLockable __instance, BlockValue _blockValue)
             {
-                if (__instance.IsLocked() && __instance.GetOwner() == null)
+                if (__instance.IsLocked() && __instance.GetOwner() == null && __instance.Parent.TryGetSelfOrFeature<TEFeatureDoor>(out var door) && door.IsOpen())
                 {
-                    //Debug.LogErrorFormat($"(OnBlockTriggered) Annoying door: {__instance.blockValue.Block.GetBlockName()}, Position: {ToCompasPos(__instance.ToWorldPos())}");
+                    //Debug.LogErrorFormat($"(OnBlockTriggered) Annoying door: {__instance.blockValue.Block.blockName}, Position: {Helper.ToCompasPos(__instance.ToWorldPos())}");
                     __instance.SetLocked(false);
                 }
             }
         }
-    }
 
-    /// <summary>
-    /// Restores the initial locked state for closed doors during TileEntity/POI reset.
-    /// </summary>
-    [HarmonyPatch(typeof(TEFeatureDoor))]
-    [HarmonyPatch(nameof(TEFeatureDoor.OnBlockReset))]
-    public static class TEFeatureDoor_OnBlockReset
-    {
-        public static void Postfix(TEFeatureDoor __instance)
+        /// <summary>
+        /// Fix locked state once Prefab created.
+        /// Then the correct locked states will be used everywhere.
+        /// </summary>
+        [HarmonyPatch(typeof(Prefab))]
+        [HarmonyPatch(methodType: MethodType.Constructor)]
+        [HarmonyPatch([typeof(Prefab), typeof(bool)])]
+        public static class Prefab_ctor
         {
-            if (__instance.lockFeature != null && __instance.lockFeature.GetOwner() == null && !IsDoorOpen(__instance.blockValue.meta))
+            public static void Postfix(Prefab __instance)
             {
-                //Debug.LogErrorFormat($"(OnBlockReset) Annoying door: {__instance.blockValue.Block.GetBlockName()}, Position: {ToCompasPos(__instance.ToWorldPos())}");
-                __instance.lockFeature.SetLocked((__instance.blockValue.meta & 4) > 0);
+                foreach (var te in __instance.tileEntities)
+                {
+                    if (te.Value is TileEntityComposite composite)
+                    {
+                        if (composite.TryGetSelfOrFeature(out TEFeatureDoor door) && door.IsOpen() && door.lockFeature != null && door.lockFeature.IsLocked())
+                        {
+                            door.lockFeature.SetLocked(false);
+                        }
+                    }
+                }
             }
         }
-    }
 
-    private static string ToCompasPos(Vector3i p)
-    {
-        return (Math.Abs(p.x).ToString() + (p.x > 0 ? "E" : "W")) + ", " + (Math.Abs(p.z).ToString() + (p.z > 0 ? "N" : "S")) + ", " + p.y.ToString() + "h";
-    }
-
-    private static string GetCallStackPath(int limit = 5)
-    {
-        var stackTrace = new System.Diagnostics.StackTrace();
-        var path = string.Join(" <-- ", stackTrace.GetFrames()
-            .Skip(3)
-            .Take(limit)
-            .Select(f => f.GetMethod())
-            .Select(m => m.DeclaringType.Name + "." + m.Name + "()"));
-        return path;
+        [HarmonyPatch(typeof(Prefab), nameof(Prefab.readTileEntities))]
+        public static class Prefab_readTileEntities
+        {
+            public static void Postfix(Prefab __instance) => Prefab_ctor.Postfix(__instance);
+        }
     }
 }
